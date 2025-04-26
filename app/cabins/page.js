3d54496b52556e4eff9ff44338ba508f49e75f5a
dasith jayanandana <dasith.jayanandana@velou.com>
2025-04-26 15:15:03 +0530

    427 - Adding Interactivity With Client Components

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,4 @@
+import Counter from '../components/Counter'
 import Navigation from '../components/Navigation'
 
 export default async function Page() {
@@ -13,6 +14,8 @@ export default async function Page() {
           <li key={user.id}>{user.name}</li>
         ))}
       </ul>
+
+      <Counter users={data}/>
     </div>
   )
 }
